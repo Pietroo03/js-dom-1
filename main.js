e3f9711834d53b10fb26e9813976d24f970bb3ca
Pietroo03 <pietro.ponte.03@gmail.com>
2024-10-08 15:17:05 +0200

    add function to main.js line 6

--- main.js
+++ main.js
@@ -1,6 +1,7 @@
-const buttonEl = document.getElementById('button')
+const buttonEl = document.querySelector('.button')
 const imgOffEl = document.querySelector('.lampOff')
 
 buttonEl.addEventListener('click', () => {
     imgOffEl.classList.add(imgOffEl.src = 'img/yellow_lamp.png')
+    buttonEl.innerHTML = 'Spegni'
 })
