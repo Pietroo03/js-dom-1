151cc4cd61bda5ef0063171cbc11c657c7f217b5
Pietroo03 <pietro.ponte.03@gmail.com>
2024-10-08 16:26:06 +0200

    modified lines 5, 8 main.js

--- main.js
+++ main.js
@@ -2,10 +2,10 @@ const buttonEl = document.querySelector('.button')
 const imgOffEl = document.querySelector('.lampOff')
 
 buttonEl.addEventListener('click', () => {
-    if (buttonEl.innerHTML = 'Accendi') {
+    if (buttonEl.innerHTML === 'Accendi') {
         imgOffEl.classList.add(imgOffEl.src = 'img/yellow_lamp.png')
         buttonEl.innerHTML = 'Spegni'
-    } else if (buttonEl.innerHTML = 'Spegni') {
+    } else if (buttonEl.innerHTML === 'Spegni') {
         imgOffEl.classList.add(imgOffEl.src = 'img/white_lamp.png')
         buttonEl.innerHTML = 'Accendi'
     }
