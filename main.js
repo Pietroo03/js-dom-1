5b25ff448eb7973a6b1b8727367574ba4d847e0e
Pietroo03 <pietro.ponte.03@gmail.com>
2024-10-08 16:21:12 +0200

    add else if to function

--- main.js
+++ main.js
@@ -2,6 +2,14 @@ const buttonEl = document.querySelector('.button')
 const imgOffEl = document.querySelector('.lampOff')
 
 buttonEl.addEventListener('click', () => {
-    imgOffEl.classList.add(imgOffEl.src = 'img/yellow_lamp.png')
-    buttonEl.innerHTML = 'Spegni'
+    if (buttonEl.innerHTML = 'Accendi') {
+        imgOffEl.classList.add(imgOffEl.src = 'img/yellow_lamp.png')
+        buttonEl.innerHTML = 'Spegni'
+    } else if (buttonEl.innerHTML = 'Spegni') {
+        imgOffEl.classList.add(imgOffEl.src = 'img/white_lamp.png')
+        buttonEl.innerHTML = 'Accendi'
+    }
 })
+
+
+
